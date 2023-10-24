aa2badd030491e025274358bbceb48f19a0d0dcb
Yuha Yabuki <zeal22300050@fsgcl.jp>
2023-10-24 10:16:25 +0900

    移動回数を取得できるようにした

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private Grid mapGrid; // マップグリッド
 
+    private int moveCount; // 移動した回数を保存する変数
+
     private Vector2 moveDistance; // 一度に移動する距離
     private Vector3 arrowKeyInput; // 矢印キーの入力を取得する変数
 
@@ -14,7 +16,7 @@ public class PlayerController : MonoBehaviour
     private bool oldFrameKeyInput; // 前フレームのキー入力を保存するフラグ
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         // 最初は初期位置を保存
         oldPosition = transform.position;
@@ -23,7 +25,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         // 矢印キーの入力を取得
         arrowKeyInput = new Vector3(Input.GetAxisRaw("Horizontal") * moveDistance.x, Input.GetAxisRaw("Vertical") * moveDistance.y, 0);
@@ -33,12 +35,13 @@ public class PlayerController : MonoBehaviour
         {
             oldPosition = transform.position; // 現在の位置を保存する
             transform.position += arrowKeyInput; // 移動する
+            moveCount++; // 移動回数を増やす
         }
         oldFrameKeyInput = ArrowKeyInput(); // 今フレームのキー入力を保存
     }
 
     // 矢印キーが押されているかどうかを返す関数
-    bool ArrowKeyInput()
+    private bool ArrowKeyInput()
     {
         // 矢印キーのいずれかが押されたなら
         if (Input.GetKey(KeyCode.RightArrow) || 
@@ -59,5 +62,11 @@ public class PlayerController : MonoBehaviour
     {
         // コリジョンが衝突したら、保存された位置に戻る
         transform.position = oldPosition;
+    }
+
+    // 現在の移動回数を取得する関数
+    public int GetCurrentMoveCount()
+    {
+        return moveCount;
     }
 }
